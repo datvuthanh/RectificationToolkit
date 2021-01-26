9286928461db00d4bbfbdee9676de016e8db1f87
Thanh Dat Vu <44630913+datvuthanh@users.noreply.github.com>
2021-01-27 04:33:39 +0700

    Update for kitti2015 dataset

--- doRectify.m
+++ doRectify.m
@@ -1,4 +1,4 @@
-function  [I1r, I2r, bb1, bb2, Pn1, Pn2, H1, H2]  = doRectify(img1, img2, calibrated)
+function doRectify(img1, img2, calibrated,id)
    %DORECTIFY Epipolar rectification outermost wrapper
    %
    % img1 and img2 are the filenames of the two images to be rectified
@@ -37,7 +37,59 @@ function  [I1r, I2r, bb1, bb2, Pn1, Pn2, H1, H2]  = doRectify(img1, img2, calibr
         
         disp('Uncalibrated: computing F from SIFT matches')
         [ml,mr,~]= sift_match_pair(I1,I2,'F');
-     
+        
+        % Dat Vu
+        firstIter = true;
+
+        for i = 1:size(ml,2)
+            %fprintf("DAT VU %d",i)
+            p1 = round(ml(:,i));
+            p2 = round(mr(:,i));
+            
+            %fprintf("TOA DO: %d %d \n",p1(1),p1(2));
+            % Create patch
+            [m,n,d] = size(I1);
+            
+            patch_size = 9;
+            if p1(1) - 4 + patch_size < n && p1(2) + 5 + patch_size < m && p2(1) - 4 + patch_size < n && p2(2) + 5 + patch_size < m
+                if p1(1) - 4 != 0 && p1(2) + 5 != 0 && p2(1) - 4  != 0 && p2(2) + 5 != 0
+                    l = imcrop(I1,[p1(1)-4,p1(2)+5,patch_size-1,patch_size-1]);
+                    r = imcrop(I2,[p2(1)-4,p2(2)+5,patch_size-1,patch_size-1]);
+
+                    
+                    %[h1,w1,c1] = size(l)
+                    save_path = sprintf('examples/ieu/kitti2015');
+                    if ~exist(save_path, 'dir')
+                        mkdir(save_path);
+                    end
+                    %img_rec0 = sprintf('%s/rec_0.png', save_path);
+                    %img_rec1 = sprintf('%s/rec_1.png', save_path);
+                    test_patch = sprintf('%s/training_%06d_%d.png', save_path,id,i);
+                    if isequal(size(l),[patch_size,patch_size,3]) && isequal(size(r),[patch_size,patch_size,3])
+                        %fprintf("DUNG");
+
+                        %a = [l,r];
+                        %a = zeros(1,2,patch_size,patch_size,3);
+                        % a(1,1,:,:,:) = l;
+                        % a(1,2,:,:,:) = r;
+                        % if firstIter == true
+                        %     final = a;
+                        %     firstIter = false;
+                        % else
+                        %     final = cat(1,final,a);
+                        % test = squeeze(a(1,2,:,:,:));
+                        %disp(a);
+                        a = [l r];
+                        imwrite(mat2gray(a), test_patch);
+                        % imwrite(mat2gray(a), img_rec1);
+                        % fprintf("A: %d %d %d %d %d",size(test));
+                    end
+                end
+            end           
+        end
+        %fprintf("A: %d %d %d %d %d",size(final));
+
+        %%
         [H1,H2, K] = rectifyF(ml, mr, [size(I1,2),size(I1,1)] );
 
         % transform left and right points
@@ -48,19 +100,17 @@ function  [I1r, I2r, bb1, bb2, Pn1, Pn2, H1, H2]  = doRectify(img1, img2, calibr
         fprintf('Rectification Sampson RMSE: %0.5g pixel \n',err);
          
         % projective MPP (Euclidean only if K is guessed right)
-        Pn1=[K,[0;0;0]]; Pn2=[K,[1;0;0]];
+        % Pn1=[K,[0;0;0]]; Pn2=[K,[1;0;0]];
         
     end
     
 %    [I1r,I2r, bb1, bb2] = imrectify(I1,I2,H1,H2,'crop');
-[I1r,I2r, bb1, bb2] = imrectify(I1,I2,H1,H2,'valid');
+    % [I1r,I2r, bb1, bb2] = imrectify(I1,I2,H1,H2,'valid');
     
-    % xshift =  bb1(1)  - bb2(1)
+    % % xshift =  bb1(1)  - bb2(1)
     
-    % fix the MPP after centering
-    Pn1 = [1 0 -bb1(1);  0 1 -bb1(2); 0 0 1] *Pn1;
-    Pn2 = [1 0 -bb2(1);  0 1 -bb2(2); 0 0 1] *Pn2;
+    % % fix the MPP after centering
+    % Pn1 = [1 0 -bb1(1);  0 1 -bb1(2); 0 0 1] *Pn1;
+    % Pn2 = [1 0 -bb2(1);  0 1 -bb2(2); 0 0 1] *Pn2;
     
 end
-
-
